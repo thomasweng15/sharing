2a387c759c12dc154bd6d476921c67d3de9026f0
thweng <thweng@microsoft.com>
2017-05-10 15:21:29 -0700

    Add ejs and initial view

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+// views as directory for all template files
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// instruct express to serve up static assets
+app.use(express.static('public'));
+
+// set routes
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
 // Set server port
 app.listen(4000);
 console.log('server is running');
